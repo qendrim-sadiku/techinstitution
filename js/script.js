5a5034547880e2bd61a5dc354d8219f25e2b1e13
qendrim-sadiku <denisadiki@gmail.com>
2017-11-23 10:27:11 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+   function tryConsole() { 
+        console.log("hello world"); 
+    } 
+
+    function showName(name, status){
+        console.log(name, status);
+    };
+    showName("Arta","studente");
